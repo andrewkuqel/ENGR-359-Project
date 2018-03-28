425a2daf95bfbcae282aea01905a58873b0c37cb
matthewchan15 <33464965+matthewchan15@users.noreply.github.com>
2018-03-28 01:37:10 -0600

    Add files via upload

---
+++ Reciever.ino
@@ -0,0 +1,67 @@
+#include <nRF24L01.h>
+#include <printf.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <Servo.h>
+
+int thumb;
+int finger;
+
+RF24 receive (7,8);                         
+const byte address [5] = {"00001"};
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+
+struct package
+  {
+    float thumb = 0;
+    float temp = 0;
+  };
+
+typedef struct package Package;
+Package data;
+
+void setup() {
+  Serial.begin(9600);
+  receive.begin();
+  receive.openReadingPipe(0,address);      
+  receive.setPALevel(RF24_PA_MIN);        
+  receive.setDataRate(RF24_250KBPS);      
+  receive.setChannel(100);                
+  receive.startListening(); 
+
+  servo1.attach(9); //finger 1 need pwm pins
+  servo2.attach(6); //finger 2
+  servo3.attach(5); //finger 3
+  servo4.attach(10); //finger 4
+  servo5.attach(3); //Thumb
+  }
+
+void loop() {
+
+  if (receive.available())                //check when received data available
+  {
+    receive.read(&data, sizeof(data));
+    thumb=data.thumb;
+    finger=data.temp;
+    servo1.write(finger);
+    servo2.write(finger);
+    servo3.write(finger);
+    servo4.write(finger);
+    servo5.write(thumb);
+    
+    
+  }
+  delay(100);
+    
+}
+
+void hello(){
+
+}
+}
+
---
+++ Sender.ino
@@ -0,0 +1,43 @@
+#include <nRF24L01.h>
+#include <printf.h>
+#include <RF24.h>
+#include <RF24_config.h>
+
+const int pinfive = 5;
+const int pinsix = 6;
+int angleValuea;
+int angleValueb;
+
+RF24 transmit (7,8);
+const byte address [5] = {"00001"};
+
+struct package
+  {
+    float thumb = 0;
+    float finger = 0;
+  };
+
+typedef struct package Package;
+Package data;
+
+void setup() {
+  transmit.begin();
+  transmit.openWritingPipe(address);            //open writing pipe to address 00001
+  transmit.setPALevel(RF24_PA_MAX);             //set RF power output to maximum
+  transmit.setDataRate(RF24_250KBPS);           //set datarate to 250kbps
+  transmit.setChannel(100);                     //set frequency to channel 100
+  transmit.stopListening();
+   pinMode(pinsix,OUTPUT);
+  digitalWrite(pinsix,HIGH);
+   pinMode(pinfive,OUTPUT);
+  digitalWrite(pinfive,HIGH);
+  }
+
+void loop() {
+ angleValuea = analogRead(A0);
+   data.thumb= map(angleValuea,446,464,0,180);
+ angleValueb = analogRead(A1);
+  data.finger= map(angleValueb,485,520,0,180);
+  transmit.write(&data,sizeof(data));
+  delay(200);                                 
+}
